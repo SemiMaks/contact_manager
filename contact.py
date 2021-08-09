a8ab6c884bf8b094d35ba8e8a420308b999bbe90
Maksim <semimaks@mail.ru>
2021-08-09 16:10:03 +0300

    Добавлен модуль, который содержит контактную информацию.

---
+++ contact.py
@@ -0,0 +1,37 @@
+# Класс Contact содержит контактную информацию.
+
+class Contact:
+    # Метод __init__ инициализирует атрибуты.
+    def __init__(self, name, phone, email):
+        self.__name = name
+        self.__phone = phone
+        self.__email = email
+
+    # Метод set_name устанавливает атрибут name.
+    def __set_name(self, name):
+        self.__name = name
+
+    # Метод set_phone устанавливает атрибут phone.
+    def __set_phone(self, phone):
+        self.__phone = phone
+
+    # Метод  set_email устанавливает атрибут email.
+    def __set_email(self, email):
+        self.__email = email
+
+    # Метод get_name возвращает атрибут name
+    def get_name(self):
+        return self.__name
+
+    # Метод get_phone возвращает атрибут phone.
+    def get_phone(self):
+        return self.__phone
+
+    # Метод get_email возвращает атрибут email.
+    def get_email(self):
+        return self.__email
+
+    def __str__(self):
+        return "Имя: " + self.__name + '\nТелефон: ' + \
+               self.__phone + '\nЭлектронная почта: ' + \
+               self.__email
